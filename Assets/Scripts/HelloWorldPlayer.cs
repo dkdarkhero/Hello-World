36ac2be32e843667ba489bca4ff0bc999df06f77
Mikkel Laurits Jakobsen <mikk514k@student.eadania.dk>
2022-04-26 16:16:18 +0200

    changed

--- Assets/Scripts/HelloWorldPlayer.cs
+++ Assets/Scripts/HelloWorldPlayer.cs
@@ -6,6 +6,11 @@ namespace HelloWorld
     public class HelloWorldPlayer : NetworkBehaviour
     {
         public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+        public bool forward = true;
+        public int TurnValue = 450;
+        public int currentTUrn = 0;
+        public float spawnTimer;
+        public GameObject player;
 
         public override void OnNetworkSpawn()
         {
@@ -42,6 +47,45 @@ namespace HelloWorld
 
         void Update()
         {
+            if (NetworkManager.Singleton.IsServer)
+            {
+
+                spawnTimer += Time.deltaTime;
+
+                if (spawnTimer > 5)
+                {
+                    spawnTimer = 0;
+                  GameObject go = Instantiate(player, new Vector3(Random.Range(-1f,1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)), gameObject.transform.rotation);
+                    go.GetComponent<NetworkObject>().Spawn();
+                }
+
+                Debug.Log("Server");
+
+                if ( forward == true)
+                {
+                    Position.Value += new Vector3(0, 0.01f , 0);
+                    currentTUrn++;
+                }
+
+                if (forward == false)
+                {
+                    Position.Value -= new Vector3(0, 0.01f, 0);
+                    currentTUrn--;
+                }
+
+
+                if (currentTUrn > TurnValue)
+                {
+                    forward = false;
+                }
+
+                if (currentTUrn < 0)
+                {
+                    forward = true;
+                }
+               
+
+            }
             transform.position = Position.Value;
         }
     }
